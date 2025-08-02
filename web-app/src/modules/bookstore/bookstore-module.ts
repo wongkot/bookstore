9e8aa7652af132b9435f42da6492e964d660e09e
Wongkot <wongkot.v@gmail.com>
2025-08-03 01:56:19 +0700

    Add book order  to navbar + create sample order page

--- web-app/src/modules/bookstore/bookstore-module.ts
+++ web-app/src/modules/bookstore/bookstore-module.ts
@@ -5,11 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookItem } from '@app/modules/bookstore/components/book-item/book-item';
 import { BookList } from '@app/modules/bookstore/components/book-list/book-list';
 import { StoreOwnerSelection } from '@app/modules/bookstore/components/store-owner-selection/store-owner-selection';
+import { BookOrders } from '@app/modules/bookstore/pages/book-orders/book-orders';
 import { Bookstore } from '@app/modules/bookstore/pages/bookstore/bookstore';
 import { SharedModule } from '@app/shared/shared-module';
 
 const ROUTES: Routes = [
 	{ path: '', pathMatch: 'full', component: Bookstore },
+	{ path: 'orders', component: BookOrders },
 ];
 
 @NgModule({
@@ -18,6 +20,7 @@ const ROUTES: Routes = [
   		BookList,
     	BookItem,
      	StoreOwnerSelection,
+		BookOrders,
 	],
 	imports: [
 		RouterModule.forChild(ROUTES),
---
+++ web-app/src/modules/bookstore/pages/book-orders/book-orders.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-orders',
+  standalone: false,
+  templateUrl: './book-orders.html',
+  styleUrl: './book-orders.sass'
+})
+export class BookOrders {
+
+}
